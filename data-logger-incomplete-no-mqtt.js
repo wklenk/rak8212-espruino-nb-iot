4df1ba1a4f20e99b9f961324a87ff0c0080337ea
Wolfgang Klenk <wolfgang.klenk@gmail.com>
2018-09-25 22:38:40 +0200

    Removed MQTT code.

--- data-logger-incomplete-no-mqtt.js
+++ data-logger-incomplete-no-mqtt.js
@@ -1,5 +1,3 @@
-require("AT");
-
 var bme280;
 var bg96nb1;
 var debug = true;
@@ -12,13 +10,6 @@ var connection_options = {
   operator: "26201"
 };
 
-var mqtt_options = {
-  // From Cayenne myDevices
-  client_id: "8b4a0650-b9df-11e8-bf81-6b1a7e6fd7d2",
-  username: "17ab1f60-df5e-11e7-8123-07faebe02555",
-  password: "2776ac0e7b1ee6d84a59fe3295b9a9d0a73a7d08",
-  keep_alive: 60
-};
 
 // Setup external hardware.
 // This is also called by "onInit()" on reboot of the device
@@ -36,50 +27,6 @@ function setupExternalHardware(cb) {
   });
 }
 
-mqtt = require("MQTT").create("mqtt.mydevices.com", mqtt_options);
-
-// As NB-IoT can be quite slow, we need to increase the connect timeout.
-mqtt.C.CONNECT_TIMEOUT = 30000;
-
-mqtt.on('ping_reply', function () {
-  if (debug) console.log("MQTT: Ping reply");
-
-  logTemperature();
-  logPressure();
-  logHumidity();
-});
-
-mqtt.on('error', function (message) {
-  if (debug) console.log("MQTT: error:", message);
-});
-
-mqtt.on('connected', function () {
-  if (debug) console.log("MQTT: Client connected.");
-});
-
-mqtt.on('disconnected ', function () {
-  if (debug) console.log("MQTT: Disconnected.");
-});
-
-
-function logTemperature() {
-  var topic = "v1/" + mqtt_options.username + "/things/" + mqtt_options.client_id + "/data/01";
-  var message = "temp,c=" + bme280.getData().temp;
-  mqtt.publish(topic, message);
-}
-
-function logPressure() {
-  var topic = "v1/" + mqtt_options.username + "/things/" + mqtt_options.client_id + "/data/02";
-  var message = "bp,hpa=" + bme280.getData().pressure;
-  mqtt.publish(topic, message);
-}
-
-function logHumidity() {
-  var topic = "v1/" + mqtt_options.username + "/things/" + mqtt_options.client_id + "/data/03";
-  var message = "rel_hum,p=" + bme280.getData().humidity;
-  mqtt.publish(topic, message);
-}
-
 
 function startDataLogger() {
   bg96nb1.openSocket(function (client) {
@@ -89,8 +36,8 @@ function startDataLogger() {
       connection_options.port,
       "established.");
 
-    if (debug) console.log("Connecting MQTT Client ...");
-    mqtt.connect(client);
+    //if (debug) console.log("Connecting MQTT Client ...");
+    //mqtt.connect(client);
   });
 }
 
